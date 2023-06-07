fb43cb1739a47363ab28c8e81512f49ff382f317
Hameedullah Asadi <hameedasade20@gmail.com>
2023-06-07 14:27:37 -0700

    changed the hardcoded variables to the user's input.

--- PackageShippingConsoleApp/PackageShippingConsoleApp/Program.cs
+++ PackageShippingConsoleApp/PackageShippingConsoleApp/Program.cs
@@ -10,39 +10,31 @@ namespace PackageShippingConsoleApp
     {
         static void Main()
         {
-            int packageWeight = 50;
-            int packageWidth = 1;
-            int packageHeight = 2;
-            int packagelength = 50;
-
-
+          
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below:"); //Prints the welcome message to the console app
             Console.WriteLine("Please enter the pakage weight.");   //Prints to the console app which directs the user
             int weight = Convert.ToInt32(Console.ReadLine());   //Converts the user's input to int32 data type
-           if (weight > packageWeight)    //This is branching which evaluates if the weight is more than packageWeight
+           if (weight > 50)    //This is branching which evaluates if the weight is more than packageWeight
             {
                 Console.WriteLine("We are sorry, the package is too heavy to be shipped via Package Express. Have a good day."); //It will print if the if is true
             }
-
 
            //PACKAGE WIDTH
             Console.WriteLine("Please enter the package width.");   //Prints to the console app which directs the user 
             int  width = Convert.ToInt32(Console.ReadLine());    //Converts the user's input to string
 
-
             //PACKAGE HEIGHT
             Console.WriteLine("please enter the package height.");  //Prints to the console app which directs the user 
-            string height = Convert.ToString(Console.ReadLine());   //Converts the user's input to string
-
+            int height = Convert.ToInt32(Console.ReadLine());   //Converts the user's input to string
 
             //PACKAGE LENGTH
             Console.WriteLine("Please enter the package length.");  //Prints to the console app which directs the user 
             int length = Convert.ToInt32(Console.ReadLine());   //Converts the user's input to int32 data type
-            if (length > packagelength)      //This is branching which evaluates if  the length is more than packagelength
+            if (length > 50)      //This is branching which evaluates if  the length is more than packagelength
             {
                 Console.WriteLine("We are sorry, the package is too big to be shipped via Package Express"); //It will print if the if is true
             }
-            decimal quote = (packageHeight * packageWidth * packagelength) * packageWeight / 100;
+            decimal quote = (weight * width * length) * weight / 100;
 
             Console.WriteLine("Your estimated total for shipping this package is: $ " + quote + ". " + " \nHave a safe trip. \nThank you!"); //Prints the total amount to be paid
             Console.ReadLine(); //Keeps the console open
